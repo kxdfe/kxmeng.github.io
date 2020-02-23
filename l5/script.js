d0a8d5ce8743f9e79c8cf363990a066433d362e2
Kevin <kxm@kcs.org.uk>
2020-02-23 21:24:11 +0000

    Create script.js

---
+++ l5/script.js
@@ -0,0 +1,110 @@
+var surdstext = "<h2>Surds and indices</h2><ul><li></li></ul>";
+var recurringtext = "<h2>Recurring decimals</h2><ul><li></li></ul>";
+var bracketstext = "<h2>Expanding brackets</h2><ul><li></li></ul>";
+var quadraticstext = "<h2>Quadratics</h2><ul><li></li></ul>";
+var trigonometrytext = "<h2>Trigonometry</h2><ul><li></li></ul>";
+var sinecosinetext = "<h2>Sine, cosine rules</h2><ul><li></li></ul>";
+var areatext = "<h2>Area of triangles</h2><ul><li></li></ul>";
+var triggraphstext = "<h2>Trigonometric graphs</h2><ul><li></li></ul>";
+var proportiontext = "<h2>Proportion</h2><ul><li></li></ul>";
+var rearrangingtext = "<h2>Rearranging formulae</h2><ul><li></li></ul>";
+var algfractionstext = "<h2>Algebraic fractions</h2><ul><li></li></ul>";
+var algequationstext = "<h2>Algebraic equations</h2><ul><li></li></ul>";
+var circlestext = "<h2>Circle theorems</h2><ul><li></li></ul>";
+var chordstext = "<h2>Intersecting chords</h2><ul><li></li></ul>";
+var constructionstext = "<h2>Constructions</h2><ul><li></li></ul>";
+var setstext = "<h2>Sets</h2><ul><li></li></ul>";
+var functionstext = "<h2>Functions</h2><ul><li></li></ul>";
+var geometrytext = "<h2>Coordinate geometry</h2><ul><li></li></ul>";
+var graphstext = "<h2>(Non) linear graphs</h2><ul><li></li></ul>";
+var graphicaltext = "<h2>Graphical solutions</h2><ul><li></li></ul>";
+var vectorstext = "<h2>Vectors</h2><ul><li></li></ul>";
+var transformationstext = "<h2>Transformations of trig graphs</h2><ul><li></li></ul>";
+
+$("#surds").click(function() {
+	$("#content").html(surdstext);
+});
+
+$("#recurring").click(function() {
+	$("#content").html(recurringtext);
+});
+
+$("#brackets").click(function() {
+	$("#content").html(bracketstext);
+});
+
+$("#quadratics").click(function() {
+	$("#content").html(quadraticstext);
+});
+
+$("#trigonometry").click(function() {
+	$("#content").html(trigonometrytext);
+});
+
+$("#sinecosine").click(function() {
+	$("#content").html(sinecosinetext);
+});
+
+$("#area").click(function() {
+	$("#content").html(areatext);
+});
+
+$("#triggraphs").click(function() {
+	$("#content").html(triggraphstext);
+});
+
+$("#proportion").click(function() {
+	$("#content").html(proportiontext);
+});
+
+$("#rearranging").click(function() {
+	$("#content").html(rearrangingtext);
+});
+
+$("#algfractions").click(function() {
+	$("#content").html(algfractionstext);
+});
+
+$("#algequations").click(function() {
+	$("#content").html(algequationstext);
+});
+
+$("#circles").click(function() {
+	$("#content").html(circlestext);
+});
+
+$("#chords").click(function() {
+	$("#content").html(chordstext);
+});
+
+$("#constructions").click(function() {
+	$("#content").html(constructionstext);
+});
+
+$("#sets").click(function() {
+	$("#content").html(setstext);
+});
+
+$("#functions").click(function() {
+	$("#content").html(functionstext);
+});
+
+$("#geometry").click(function() {
+	$("#content").html(geometrytext);
+});
+
+$("#graphs").click(function() {
+	$("#content").html(graphstext);
+});
+
+$("#graphical").click(function() {
+	$("#content").html(graphicaltext);
+});
+
+$("#vectors").click(function() {
+	$("#content").html(vectorstext);
+});
+
+$("#transformations").click(function() {
+	$("#content").html(transformationstext);
+});
