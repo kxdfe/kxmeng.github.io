5a90a3774bab3d6e1f196371685b5e0a5a06999d
Kevin <kxm@kcs.org.uk>
2020-03-08 22:44:50 +0000

    x

--- y8/script.js
+++
@@ -1,6 +0,0 @@
-$("#sidebar-left li").click(function() {
-	$('.sidebar-active').removeClass('sidebar-active');
-	$(this).addClass('sidebar-active');
-	$('.active-topic').removeClass('active-topic');
-	$('div#' + $(this).attr('id')).addClass('active-topic');
-});
